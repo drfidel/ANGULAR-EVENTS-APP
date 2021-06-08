f811dff5d1efd600baddd2fd072b67228be72c9c
drfidel <akiyofidel@gmail.com>
2021-06-08 20:40:46 +0300

    add formvalidators and error messages

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { AuthService } from './auth.service'
 import { Router } from '@angular/router'
 
 @Component({
-  templateUrl: './profile.component.html'
+  templateUrl: './profile.component.html',
+  styles: [`
+    em {float:right; color:#E05C65; padding-left: 10px; }
+    .error input {background-color: #E3C3C5;}
+    .error ::-webkit-input-placeholder {color:#999;}
+    .error ::-moz-placeholder {color:#999;}
+    .error :-moz-placeholder {color:#999;}
+    .error :-ms-input-placeholder {color:#999;}
+    `]
 })
 export class ProfileComponent implements OnInit{
 
@@ -15,8 +23,8 @@ export class ProfileComponent implements OnInit{
   }
   
   ngOnInit(){
-    let firstName = new FormControl(this.authService.curretUser.firstName)
-    let lastName = new FormControl(this.authService.curretUser.lastname)
+    let firstName = new FormControl(this.authService.curretUser.firstName, Validators.required)
+    let lastName = new FormControl(this.authService.curretUser.lastname, Validators.required)
 
     this.profileForm = new FormGroup({
       firstName: firstName,
@@ -25,8 +33,20 @@ export class ProfileComponent implements OnInit{
   }
 
   saveProfile(formValues: { firstName: string; lastName: string }) {
-    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
-    this.router.navigate(['events'])
+    if (this.profileForm.valid){
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+      this.router.navigate(['events'])
+    }
+  }
+
+  validatefirstName(){
+    return this.profileForm.controls.firstName.valid || 
+    this.profileForm.controls.firstName.untouched
+  }
+
+  validatelastName(){
+    return this.profileForm.controls.lastName.valid || 
+    this.profileForm.controls.lastName.untouched
   }
 
   cancel(){
