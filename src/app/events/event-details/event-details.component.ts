9cef08c87b9d189d35888a4b34118e5ac9249a9e
drfidel <akiyofidel@gmail.com>
2021-07-18 21:04:43 +0300

    added datasorting

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -17,6 +17,7 @@ export class EventDetailsComponent implements OnInit {
     event:any
     addMode?:Boolean
     filterBy:string = 'all'
+    sortBy:string = 'votes'
 
     constructor(private eventService:EventService, private route:ActivatedRoute) { }
 
--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -12,11 +12,14 @@ export class SessionListComponent implements OnChanges {
 
     @Input() sessions:ISession[] | undefined
     @Input() filterBy:string | undefined
+    @Input() sortBy:string | undefined
     visibleSessions:ISession[] | undefined = []
 
     ngOnChanges(){
         if(this.sessions){
             this.filterSessions(this.filterBy)
+            this.sortBy === 'name' ? this.visibleSessions?.sort(sortByNameAsc) : 
+            this.visibleSessions?.sort(sortByVotesDesc)
         }
     }
 
@@ -29,4 +32,15 @@ export class SessionListComponent implements OnChanges {
             })
         }
     }
+
+}
+
+function sortByNameAsc(s1: ISession, s2:ISession){
+    if(s1.name > s2.name) return 1
+    else if (s1.name === s2.name) return 0
+    else return -1
+}
+
+function sortByVotesDesc(s1: ISession, s2:ISession){
+    return s2.voters.length - s1.voters.length
 }
